3a40f820fb04b960811e907018b3512d6ee3ca03
Mario Sergio <mario.junior23@univille.br>
2025-02-15 18:27:13 -0300

    tela da musica com o player e o banner da musica

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay,faBackwardStep, faForwardStep } from "@fortawesome/free-solid-svg-icons";
 
 const Player = () => {
   return (
-    <div className='player'>Player</div>
+    <div className='player'>
+
+      <div className="player__controllers">
+      
+        <FontAwesomeIcon className="player__icon" icon={faBackwardStep}/>
+
+        <FontAwesomeIcon className="player__icon player__icon--play" icon={faCirclePlay}/>
+
+        <FontAwesomeIcon className="player__icon" icon={faForwardStep}/>
+        
+      </div>
+
+
+      <div className="player__progress">
+        <p>00:00</p>
+
+        <div className='player__bar'>
+
+
+          <div className='player__bar-progress'> </div>
+        
+        
+        </div>
+
+
+        
+        <p>00:00</p>
+
+
+      </div>
+
+
+    </div>
   )
 }
 
