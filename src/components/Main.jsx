e79fd7cfdb5afc58edc19e84c890f96bdc91030d
Mario Sergio <mario.junior23@univille.br>
2025-02-15 16:15:50 -0300

    componentes adicionados na pagina de musica e artista

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,25 +5,37 @@ import ItemList from "./ItemList";
 import { artistArray } from "../assets/database/artists";
 import { songsArray } from "../assets/database/songs";
 
-const Main = () => {
+const Main = ({type}) => {
   
 
 
   return (
     <div className="main">
       {/* Item List de Artistas */}
-      <ItemList title = "Artistas" 
-      items = {5}
+
+      {type === "artists"  || type === undefined ? <ItemList title = "Artistas" 
+      items = {10}
       itemsArray = { artistArray } 
       path = "/artists" 
-      idPath = "/artist" />
+      idPath = "/artist" /> :
+       (
+       <></>
+       ) }
+
+      
       
       {/* Item List de Musicas */}
+
+      {type === "songs" || type === undefined  ?
       <ItemList title ="Musicas" 
-      items = {10} 
+      items = {20} 
       itemsArray = { songsArray } 
       path = "/songs" 
-      idPath = "/song" />
+      idPath = "/song" /> :
+      (
+      <></>
+      ) } 
+      
     </div>
   );
 };
