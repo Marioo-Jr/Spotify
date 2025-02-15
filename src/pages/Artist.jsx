3dea3458f3caf91e8e922be5bf62bb8f5ad71797
Mario Sergio <mario.junior23@univille.br>
2025-02-15 17:29:15 -0300

    foi adicionado uma tela para cada musica e tambem uma tela para mostrar as musicas do artista

--- src/pages/Artist.jsx
+++ src/pages/Artist.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+import { Link } from 'react-router-dom';
+import SongList from '../components/SongList';
 
 const Artist = () => {
   return (
-    <div>Artist</div>
+    <div className='artist' >
+
+      <div className="artist__header" style={{backgroundImage:
+        "linear-gradient(to bottom, var(--_shade), var(--_shade)), url(https://www.hashtagtreinamentos.com/wp-content/uploads/2025/02/mc-tuto.png)"
+       }}
+      >
+
+        <h2 className='artist__title'>Jorge & mateus </h2>
+
+      </div>
+
+      <div className="artist__body">
+
+        <h2>Populares</h2>
+        <SongList/>
+      </div>
+
+      <Link to = "/song/">
+        <FontAwesomeIcon className="single-item__icon single-item__icon--artist "icon={faCirclePlay}/>
+      </Link>
+
+    </div>
   )
 }
 
--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import Player from '../components/Player'
 
 const Song = () => {
   return (
-    <div>Song</div>
+    <div className='song' >
+
+      <div className="song__container">
+        <div className='song__image-container'>
+
+          <img src="https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24" alt="imagem da musica x" />
+
+        </div>
+
+      </div>
+
+      <div className="song__bar">
+        
+        
+        <div className='song__artist-image'>
+          <img
+           width={75}
+           height={75}
+           src="https://i.scdn.co/image/ab6761610000517477937baabc49dea13c17c706" alt="imagem do artista Y" />
+        </div>
+
+        <Player/> 
+        <div>
+          <p className='song_name' >Última Saudade - Ao Vivo</p>
+          <p>Henrique & Juliano</p>
+        </div>
+
+
+      </div>
+
+
+    </div>
   )
 }
 
