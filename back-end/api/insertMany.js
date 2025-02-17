4f03e6324158ef0d2894d9e45a80d115e6052725
Mario Sergio <mario.junior23@univille.br>
2025-02-16 21:49:02 -0300

    feita a conexao do bd com a api

--- back-end/api/insertMany.js
+++ back-end/api/insertMany.js
@@ -1,9 +1,10 @@
 import {artistArray} from "../../front-end/src/assets/database/artists.js"
 import {songsArray} from "../../front-end/src/assets/database/songs.js"
+import { db } from "./connect.js";
 
 
 const newArtistArray = artistArray.map((currentArtistObj)=>{
-    const newArtistObj = {...currentArtistObj}
+    const newArtistObj = { ...currentArtistObj }
     delete newArtistObj.id
 
     return newArtistObj
@@ -11,10 +12,15 @@ const newArtistArray = artistArray.map((currentArtistObj)=>{
 });
 
 
-const newSongArray = songArray.map((currentSongObj)=>{
-    const newSongObj = {...currentSongObj}
+const newSongsArray = songsArray.map((currentSongObj)=>{
+    const newSongObj = { ...currentSongObj }
     delete newSongObj.id
 
     return newSongObj
 
 });
+
+
+
+const responseSongs = await db.collection('songs').insertMany(newSongsArray)
+const responseArtist = await db.collection('artists').insertMany(newArtistArray)
